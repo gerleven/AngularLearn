0f33f91b2b481165df1be75865256320e21af5bc
gerleven <gerleven@gmail.com>
2021-08-30 16:05:30 -0300

    asd

--- German/gerRouting/src/app/app.component.ts
+++ German/gerRouting/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1>Routing Test</h1><router-outlet></router-outlet><!--Agregamos directamente aca porque son solo 2 lineas, no tiene sentido un archivo aparte-->`,
+  template: `<h1>Routing Test</h1><router-outlet></router-outlet><!--Agregamos directamente aca porque son solo 2, lineas, no tiene sentido un archivo aparte-->`,
   styles: ['h1{color:blue}']
 })
 export class AppComponent {
