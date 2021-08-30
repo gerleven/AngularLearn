d25da7a440474f633ac4af0574c113bb1e568e69
gerleven <gerleven@gmail.com>
2021-08-30 16:18:13 -0300

    material01 upload

---
+++ German/material01/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css','./GuidesMaterial/04-Theming your oiwn Component/carousel.scss']
+})
+export class AppComponent {
+  loving=10;
+
+  
+}
+
+
