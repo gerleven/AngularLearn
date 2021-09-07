c8ed95bbc33827ef48c576f9e007c076794268cd
gerleven <gerleven@gmail.com>
2021-09-07 17:56:00 -0300

    agregue comentario explicativo importante sobre por que nos podemos .subscribte() subscribir al .getTitles() dentro del app.component.ts, porque el get.Titles() del service retorna el observable del .get() previamente modificado por el pipe y el map

--- ejem12/src/app/books.service.ts
+++ ejem12/src/app/books.service.ts
@@ -11,9 +11,11 @@ export class BooksService {
   getTitles(title: string): Observable<string[]> { //Importante poner el ": Observable<string[]>"
     let url = "https://www.googleapis.com/books/v1/volumes?q=intitle:" + title;
     return this.httpClient.get(url).pipe(     // Aca em vez de subscribirnos como haciamos antes con el httpClient ahora recibimos directamente el Observable del .get() y lo mandamos al pipe y map para manipularlo y trasformarlo al formato que queremos.
-      map(response => this.extractTitles(response as any))
+      map(response => this.extractTitles(response as any)) //aca en vez de hacer un .pipe() podriamos habernos subscribido al observable que retorna el .get() y ahccer algo dentro del response=>{}
+      //pero en vez de hacer this.httpClient.get(url).subscribe(r=>{return algo},e=>{}) directamente retornamos el observale que nos trae el .get() (previamente modificado por el pipe y el map) pero lo que estamos retornando es el observable
+      //y por es que depsues en el app.component.ts nos podemos subscribir mediante un .subscribe() al "this.service.getTitles(title)", porque ese "this.httpClient.get(url)" esta retornando un observable.
+      );
       //en el ejem13 vemos tambien como manejar el error para dar un mensaje de mas alto nivel al componente
-    );
   }
 
   private extractTitles(response) {
