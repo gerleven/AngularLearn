cecc71df9f47eb454a3ad44e334adfe5ec65d217
gerleven <gerleven@gmail.com>
2021-09-07 17:13:31 -0300

    empezando el ejer5 de repaso

---
+++ German/Repasando/ejer5mio/src/app/item/item.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Item } from './Item';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styles: [".tachado{text-decoration: line-through; color: gray; font-weight: normal;}"],
+  styleUrls: ['./item.component.css']
+})
+export class ItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+  @Input()
+  itemHijo: Item;
+
+
+  @Output()
+  emitterDelete = new EventEmitter<Item>();
+  
+  @Output() emitterCheck = new EventEmitter<Item>();
+
+  deleteItem(){
+    console.log("item que paso para eliminar: ",this.itemHijo);
+    this.emitterDelete.emit(this.itemHijo);
+  }
+
+  checkItem(){
+    this.emitterCheck.emit(this.itemHijo);
+  }
+
+}
