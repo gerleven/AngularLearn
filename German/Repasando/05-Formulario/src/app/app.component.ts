1a1defb1680115a8093156587bb29edb4576e336
German Lopez <gerleven@gmail.com>
2023-06-05 11:46:03 -0300

    formularios controlando el control desde el codigo mediante ViewChild y ElementRef

--- German/Repasando/05-Formulario/src/app/app.component.ts
+++ German/Repasando/05-Formulario/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -26,4 +26,26 @@ export class AppComponent {
   
   // 2- Acceso a los controles desde el código para leer y modificar su estado
   
+  toSaveTitleInput: string = "refInput default value";
+  itemList = ["Item1", "Item2"]
+  addItem(value: string){
+    this.itemList.push(value);
+  }
+
+  // Antes se podia inicializar asi la variable anotada con ViewChild:
+  // @ViewChild('title') titleInput: ElementRef;
+  //Pero en las versiones mas acctualiza de TypeScript ya no se puede hacer esto y hay que optar por estas 2 opciones:
+  // @ViewChild('inputRef') titleInput!: ElementRef<HTMLInputElement>;
+  // @ViewChild('title') titleInput!: ElementRef;
+  
+@ViewChild('inputRef') titleInput!: ElementRef;
+
+updateTitle(){
+  this.title = this.titleInput.nativeElement.value;
+  this.titleInput.nativeElement.value = '';
+  this.titleInput.nativeElement.focus();
+}
+  
+
+
 }
